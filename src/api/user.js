8553e50ef3e3a5796ec9f6455949063be1c0ee0f
niconiconiQWQ <1774264766@qq.com>
2022-11-11 22:05:46 +0800

    first

---
+++ src/api/user.js
@@ -0,0 +1,25 @@
+// 引入axios的二次封装
+import request from "@/utils/request";
+// 登录接口
+export function login(data) {
+  return request({
+    url: "/admin/acl/index/login",
+    method: "post",
+    data,
+  });
+}
+// 获取用户信息
+export function getInfo(token) {
+  return request({
+    url: "/admin/acl/index/info",
+    method: "get",
+    params: { token },
+  });
+}
+// 退出登录函数
+export function logout() {
+  return request({
+    url: "/admin/acl/index/logout",
+    method: "post",
+  });
+}
